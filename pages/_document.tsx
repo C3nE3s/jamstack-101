961bdfb51419000a9eed84febb5cc213df0bf384
Calin Ennis <calinennis+professionalgithub@gmail.com>
2022-06-20 20:14:30 -0400

    eslint;inter & tailwind; cleanup

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+// TODO:
+// Look into fontawesome kit
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/*Fonts*/}
+        <link
+          rel="preload"
+          href="/fonts/inter-var-latin.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        {/* <link rel="preconnect" href="https://kit.fontawesome.com" crossOrigin="anonymous" /> */}
+      </Head>
+      <body className="">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
