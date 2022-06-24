5e3dd693a025566e545125e74dc3ccce11306986
Calin Ennis <calinennis+professionalgithub@gmail.com>
2022-06-24 16:31:58 -0400

    add api for emphasized text

--- components/Text.tsx
+++ components/Text.tsx
@@ -5,18 +5,21 @@ interface TextProps {
   className?: string;
   color: "gray" | "white";
   as: "p" | "span";
+  emphasized?: boolean;
 }
 
 const Text: React.FC<PropsWithChildren<TextProps>> = (props) => {
-  const { as: Component, children, className, color } = props;
+  const { as: Component, children, className, color, emphasized } = props;
 
   const baseTextStyle = "lg:text-lg font-light";
+  const emphasizedStyle = "font-normal lg:text-2xl";
   const textColor = color === "gray" ? "text-gray-lightest" : "text-white";
 
   // Allow for classes to be passed down, but remove dupes and preserve order
+  let emphasis = emphasized ? emphasizedStyle : baseTextStyle;
   let userDefinedStyles = className ? className : "";
   let dedupedStyles = dedupeStringArray([
-    baseTextStyle,
+    emphasis,
     textColor,
     userDefinedStyles,
   ]);
